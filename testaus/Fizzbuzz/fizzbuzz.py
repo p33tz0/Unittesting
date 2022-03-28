8c8e47f7d6d136ba2dffcc4942f6d0d977bd1414
Petrus Savolainen <petrus.savolainen@gmail.com>
2022-03-28 15:39:52 +0300

    Simplified fizzbuzz

--- testaus/Fizzbuzz/fizzbuzz.py
+++ testaus/Fizzbuzz/fizzbuzz.py
@@ -1,29 +1,10 @@
-#Function that returns Fizz if number is divided by 3
-def fizz(x):
-    if x % 3 == 0:
-        return "Fizz"
-    else:
-        return x
-
-def buzz(x):
-    if x % 5 == 0:
-        return "Buzz"
-    else:
-        return x
-
-def fizzbuzz(x):
-    if x % 15 == 0:
-        return "FizzBuzz"
-    else:
-        return x
-    
 #Function that takes a number and returns Fizz, Buzz or FizzBuzz
 def num(x):
     if x % 3 == 0 and x % 5 == 0:
-        return fizzbuzz(x)
+        return "FizzBuzz"
     elif x % 3 == 0:
-        return fizz(x)
+        return "Fizz"
     elif x % 5 == 0:
-        return buzz(x)
+        return "Buzz"
     else:
         return x
